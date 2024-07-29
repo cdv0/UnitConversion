f4cefddea6706160268abfa92e2f89eeaa72ca1f
Cathleen Vu <cathleendvu@gmail.com>
2024-07-28 22:01:26 -0700

    Add a comment

--- UnitConversion/UnitConversionApp.swift
+++ UnitConversion/UnitConversionApp.swift
@@ -2,6 +2,8 @@
 //  UnitConversionApp.swift
 //  UnitConversion
 //
+//  Input the unit that you want to convert, and select the desired output unit. This application will handle the unit conversion for length.
+//
 //  Created by Cathleen Vu on 7/28/24.
 //
 
