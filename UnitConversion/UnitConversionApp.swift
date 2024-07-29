333512a571346a485393359c3f2f500c6ea6aed6
Cathleen Vu <cathleendvu@gmail.com>
2024-07-28 21:54:23 -0700

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Cathleen Vu on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
