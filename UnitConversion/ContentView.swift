52344ba9b4a9bc3f2bb08cbee72bd8b1038796b7
Cathleen Vu <cathleendvu@gmail.com>
2024-07-28 22:12:50 -0700

    Add segments for input unit and output unit

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputUnit = "meters"
+    @State private var outputUnit = "feet"
+    let units = ["meters", "kilometers", "feet", "yards", "miles"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                
+                Section("Input unit") {
+                    Picker("Input unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Output unit") {
+                    Picker("Output unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+            }
+            .navigationTitle("Unit Conversion")
         }
-        .padding()
     }
 }
 
